b02d6e45cea4b6714b83a1c92af4a927137b2ce4
Shankar Govindarajan <shgovind@redhat.com>
2019-01-28 00:28:32 -0600

    Added json stuff

--- src/main/java/com/wkshp/demo/MessageConsumer.java
+++ src/main/java/com/wkshp/demo/MessageConsumer.java
@@ -1,5 +1,7 @@
 package com.wkshp.demo;
 
+import com.google.gson.Gson;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
@@ -13,6 +15,9 @@ class MessageConsumer{
     @JmsListener(destination = "${jms.queue.destination}")
     public void processMsg(String message) {
         System.out.println("============= Received: " + message);
+        Gson g = new Gson();
+        Payment payment = g.fromJson(message, Payment.class);
+        System.out.println("============= After Payment: " + payment);
         //messageRepository.save(new Message(message));
     }
 }
