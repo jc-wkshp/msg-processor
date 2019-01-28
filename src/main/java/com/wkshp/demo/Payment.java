f8452cebc5d4389d0415b63807ee7dd09ddb8a4c
Shankar Govindarajan <shgovind@redhat.com>
2019-01-28 13:40:19 -0600

    Revert Changes

--- src/main/java/com/wkshp/demo/Payment.java
+++ src/main/java/com/wkshp/demo/Payment.java
@@ -17,7 +17,7 @@ public class Payment {
 	private Integer id;
     private String name;
     private Double amount;
-    private String details;
+    //private String details;
     
     public Payment() {}
 
@@ -26,12 +26,12 @@ public class Payment {
         this.amount = amount;
     }
 
-    
+    /*
     public Payment(String name, Double amount, String details) {
         this.name = name;
         this.amount = amount;
         this.details = details;
-    }
+    }*/
 
 	public Integer getId() {
 		return id;
@@ -57,14 +57,14 @@ public class Payment {
 		this.amount = amount;
     }
 
-    
+    /*
     public String getDetails() {
 		return details;
 	}
 
 	public void setAmount(String details) {
 		this.details = details;
-    }
+    }*/
 
     @Override
     public String toString() {
